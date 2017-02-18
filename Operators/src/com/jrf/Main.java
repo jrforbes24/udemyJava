1e94b11170b834470d804b6e1ca71bd62d98ed4e
John R. Forbes <jrforbes24@gmail.com>
2017-02-17 17:23:41 -0700

    Fun with operators.

--- Operators/src/com/jrf/Main.java
+++ Operators/src/com/jrf/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Fun with operators
-        int result = 1 + 2;
+       /* int result = 1 + 2;
         System.out.println(result);
 
         int previousResult = result;
@@ -19,5 +19,43 @@ public class Main {
 
         previousResult =  result;
         result = result / 5;
+        System.out.println( previousResult );
+        System.out.println(result);
+
+        previousResult = result;
+        result =  result % 3;
+        System.out.println( previousResult );
+        System.out.println(result);
+
+        previousResult = result;
+        result = result +1 ;
+        result++;
+
+        System.out.println("Result is now: " + result);
+
+        result--;
+        System.out.println("Result is now: " + result);
+        result+=2;
+        System.out.println("Result is now: " + result);
+        result*=10;
+        System.out.println("Result is now: " + result);
+        result-=10;
+        System.out.println("Result is now: " + result);
+        result /= 10;
+        System.out.println("Result is now: " + result);*/
+
+        boolean isAlien = false;
+        if (!isAlien)
+            System.out.println("It is not an alien!");
+
+        int topScore = 100;
+        if (topScore >= 100)
+            System.out.println("You got the high score");
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore || topScore < 100)
+            System.out.println("Greater than second top score and less then 100");
+
+
     }
 }
