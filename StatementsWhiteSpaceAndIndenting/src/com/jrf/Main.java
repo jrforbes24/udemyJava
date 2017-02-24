7fdbd28eb6033421631d33a47d30a0f318ad5eee
John R. Forbes <jrforbes24@gmail.com>
2017-02-23 21:40:47 -0700

    more java fun

---
+++ StatementsWhiteSpaceAndIndenting/src/com/jrf/Main.java
@@ -0,0 +1,24 @@
+package com.jrf;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is myVariable: " + myVariable);
+
+        System.out.println("This is" +
+                " another" +
+                " still more"
+        );
+
+        int anotherVariable = 50;
+        myVariable--;
+        anotherVariable++;
+
+        System.out.println("This is another variable " + anotherVariable);
+
+    }
+}
